e1932da015f1e0205a42519f86cbfe3197b3349e
danimirst <danimirst@gmail.com>
2020-12-07 20:48:45 -0800

    add roll_dice function

---
+++ player.js
@@ -0,0 +1,10 @@
+var Player1stats = [money = 2000, properties = [], jail = false, get_out_of_jail = 0, location = 0];
+var roll_die = function() {
+    return Math.floor((Math.random() * 6) + 1);
+};
+var roll_dice = function() {
+    return roll_die() + roll_die();
+};
+
+console.log(roll_dice());
+
