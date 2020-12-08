d0f5f6dab7d57c533c254ca2aa9f02dce070346b
danimirst <danimirst@gmail.com>
2020-12-07 19:40:53 -0800

    added board. added major 4 squares to board.

--- board.js
+++ board.js
@@ -1,13 +1,30 @@
-var path = new Path();
-path.strokeColor = 'black';
-var start = new Point(100, 100);
-path.moveTo(start);
-path.lineTo(start + [ 100, -50 ]);
-var path = new Path.Circle(view.bounds.center, 30);
-path.fillColor = 'red';
+var boardSize = view.size.height * 0.6;
+var tileHeightWidthRatio = 5/3;
+var tileWidth = boardSize / (9 + tileHeightWidthRatio * 2);
+var tileHeight = tileWidth * tileHeightWidthRatio;
 
-view.onResize = function(event) {
-    path.position = view.center;
-}
+var boardRect = new Rectangle([0,0], [boardSize, boardSize]);
+boardRect.center = view.center;
+var board = new Path.Rectangle(boardRect);
+board.strokeColor = 'black';
 
-//CCOOKIES
+
+var goRect = new Rectangle([0, 0], [tileHeight, tileHeight]);
+goRect.bottomRight = boardRect.bottomRight;
+var goSquare = new Path.Rectangle(goRect);
+goSquare.strokeColor = 'black';
+
+var go_to_jail_Rect = new Rectangle([0, 0], [tileHeight, tileHeight]);
+go_to_jail_Rect.topRight = boardRect.topRight;
+var go_to_jail_Square = new Path.Rectangle(go_to_jail_Rect);
+go_to_jail_Square.strokeColor = 'black';
+
+var parkingRect = new Rectangle([0, 0], [tileHeight, tileHeight]);
+parkingRect.topLeft = boardRect.topLeft;
+var parkingSquare = new Path.Rectangle(parkingRect);
+parkingSquare.strokeColor = 'black';
+
+var jailRect = new Rectangle([0, 0], [tileHeight, tileHeight]);
+jailRect.bottomLeft = boardRect.bottomLeft;
+var jailSquare = new Path.Rectangle(jailRect);
+jailSquare.strokeColor = 'black';
